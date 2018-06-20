6b691d9f06c369eb58b5e36eb57a11268a7a84c0
schee <rodemtree1009@naver.com>
2018-06-20 23:49:19 +0900

    [HOMEPAGE] WORK_20180620_03

--- pages/index.vue
+++ pages/index.vue
@@ -9,11 +9,31 @@
        </div> 
      </article> 
      <br> 
-      <article class="hero is-dark"> 
+      <article class="hero"> 
        <div class="hero-body"> 
          <div class="container">
+           <h1>Go to Schee's Profile!</h1>
+           <h2>Introduce Myself, bro! <nuxt-link to="/profile" class="button is-danger">Link Start!</nuxt-link></h2>
          </div> 
        </div> 
-     </article> 
+     </article>
+      <br>
+     <article class="hero is-dark"> 
+       <div class="hero-body"> 
+         <div class="container">
+           <h1>Do you want to see my work?</h1>
+           <h2>Come in, Schee's Lab! <nuxt-link to="/work" class="button is-danger">Go to Page!</nuxt-link></h2>
+           </div> 
+       </div> 
+        </article>
+         <br> 
+        <article class="hero is-light"> 
+       <div class="hero-body"> 
+         <div class="container">
+           <h1>What is Schee's Hobby?</h1>
+           <h2>Can you join us? <nuxt-link to="/hobby" class="button is-danger">Let's get it!</nuxt-link></h2>
+           </div> 
+       </div> 
+        </article>
    </article>
  </template> 
