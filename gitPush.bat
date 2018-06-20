031888e7e54b72a1dcc6c8dcfc4bfef687565210
schee <rodemtree1009@naver.com>
2018-06-20 14:28:08 +0900

    [HOMEPAGE] WORK_20180620_03

--- gitPush.bat
+++ gitPush.bat
@@ -1,4 +1,4 @@
 git add --all
-git commit -m "[HOMEPAGE] WORK_20180620_02"
+git commit -m "[HOMEPAGE] WORK_20180620_03"
 
 git push -u origin master
--- pages/hobby.vue
+++ pages/hobby.vue
@@ -1,7 +1,31 @@
 <template>
 
 <section class="section">
-<p>취미소개</p>
+
+    <div class="card">
+  <div class="card-content">
+    <p class="title">Walk Off The Earth</p>
+    <p class="subtitle">Manager: John Morley</p>
+    <p>WoteManagement@gmail.com</p>
+    <p>WOTE official channel!!</p>
+    <p>WOTE is a band. A band that is obsessed with making killer music in all its forms. Join them on their journey!</p>
+    <figure class="image is-4by1">
+            <img src="~assets/banner.jpg">
+          </figure>
+  </div>
+  <footer class="card-footer">
+    <p class="card-footer-item">
+      <span>
+        View on <a href="https://www.youtube.com/channel/UC9PEibgWOqZ-1I1JdxRmr6g">Youtube</a>
+      </span>
+    </p>
+    <p class="card-footer-item">
+      <span>
+        Share on <a href="https://www.facebook.com/walkofftheearth?fref=ts">Facebook</a>
+      </span>
+    </p>
+  </footer>
+</div>
 
 </section>
 </template>
